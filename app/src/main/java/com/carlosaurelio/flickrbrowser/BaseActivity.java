eebb7bc557eb54f475a371a0a9db5d5aafb12939
Carlos Aurélio <carlosspok@gmail.com>
2017-04-26 22:14:45 -0300

    #1 - Adicionando todo o Projeto

---
+++ app/src/main/java/com/carlosaurelio/flickrbrowser/BaseActivity.java
@@ -0,0 +1,32 @@
+package com.carlosaurelio.flickrbrowser;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Carlos Aurelio on 18/02/2016.
+ */
+public class BaseActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+    public static final String FLICKR_QUERY = "FLICKR_QUERY";
+    public static final String PHOTO_TRANSFER = "PHOTO_TRANSFER";
+
+    protected Toolbar activateToolbar() {
+        if (mToolbar == null) {
+            mToolbar = (Toolbar) findViewById(R.id.app_bar);
+            if (mToolbar != null) {
+                setSupportActionBar(mToolbar);
+            }
+        }
+        return mToolbar;
+    }
+
+    protected Toolbar activatedToolbarWithHomeEnabled() {
+        activateToolbar();
+        if (mToolbar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        return mToolbar;
+    }
+}
